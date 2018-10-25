02755544f3a1ce152e1158d51b6a4a1ad0606ef2
goyal-vi <43448555+goyal-vi@users.noreply.github.com>
2018-10-25 15:26:07 +0530

    Minor code update
    Housekeeping changes

--- Chat_Model/Sender.py
+++ Chat_Model/Sender.py
@@ -2,13 +2,13 @@
 
 import socket
 
-while 2>1:
-	x=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)	
-	msg=raw_input("Enter message:")
+while True:
+	soc_var = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)	
+	msg = input("Enter message:")
 #sendto() binds with it the address of this node. Therefore, we don't have to specify the same in order to receive a messsage.
 #Replace "Receiver_IP" with the receiver's IP address. Here, "8888" is the specified port number of receiver node.
-	x.sendto(msg,("Receiver_IP",8888))
+	soc_var.sendto(msg,("Receiver_IP",8888))
 #recvfrom() accepts the data sent over the network.
-	print x.recvfrom(100)
+	print( soc_var.recvfrom(100))
 
 
